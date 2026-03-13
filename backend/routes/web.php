eca42762f5b6db03eada0269c871d377897e7f21
shwetacodebase-hue <shweta.codebase@gmail.com>
2026-03-13 16:05:30 +0530

    commit

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/run-secret-commands-123', function () {
+    Artisan::call('optimize:clear');
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('migrate', ['--force' => true]);
+
+    return nl2br(Artisan::output());
 });
